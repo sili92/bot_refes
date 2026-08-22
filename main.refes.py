d5f9fc7527094afe6a7f7f9a248aeff66e450680
sili92 <agarrandolapalaa@gmail.com>
2026-08-22 03:44:47 +0200

    main.refes.py

--- main.refes.py
+++ main.refes.py
@@ -178,7 +178,7 @@ async def refe_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     botones = InlineKeyboardMarkup([
         [
-            InlineKeyboardButton("𝙄𝙉𝙁𝙊𝙍𝙈𝘼𝙏𝙄𝙊𝙉", url="https://t.me/infocherryspriv"),
+            InlineKeyboardButton("𝙄𝙉𝙁𝙊𝙍𝙈𝘼𝙏𝙄𝙊𝙉", url="https://t.me/infocherrys"),
             InlineKeyboardButton("𝙊𝙒𝙉𝙀𝙍", url="https://t.me/zilbato")
         ]
     ])
